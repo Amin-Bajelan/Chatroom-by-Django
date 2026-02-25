fa815d78747a60bac3850a2f7897a115ccf86852
Amin Bajelan <m.amin.bajelan@gmail.com>
2026-02-25 23:56:47 +0330

    create model message & group

--- chatroom/asgi.py
+++ chatroom/asgi.py
@@ -11,14 +11,15 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from chatroom.routing import websocket_urlpatterns  # وارد کردن مسیرهای WebSocket
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatroom.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "http": get_asgi_application(),  # درخواست‌های HTTP به سرور معمولی Django می‌روند
+    "websocket": AuthMiddlewareStack(  # WebSocket‌ها به کانال‌ها هدایت می‌شوند
         URLRouter(
-           
+            websocket_urlpatterns  # مسیرهای WebSocket که در routing.py تعریف می‌کنید
         )
     ),
 })
